e88b1c967b375db97ec268955ffd221a7c096ecc
JoyKim66 <masscom6051@naver.com>
2022-07-04 22:52:59 +0900

    로그인 기능 api 및 라우터 연결

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,37 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001",
-    headers: {
-        "content-type": "application/json;charset=utf-8",
-        accept: "application/json",
-    }
+  baseURL: "http://54.180.140.222",
+  headers: {
+    "content-type": "application/json",
+  },
 });
 
 api.interceptors.request.use(function (config) {
-    const accessToken = localStorage.getItem('jwtToken')
-    const googleToken = localStorage.getItem('googleToken')
-    if ((googleToken) !== null) {
-        config.headers.commen["Authorization"] = `Bearer ${googleToken}`;
-    }
+  const accessToken = localStorage.getItem("jwtToken");
+  const googleToken = localStorage.getItem("googleToken");
+  if (googleToken !== null) {
+    config.headers.common["Authorization"] = `Bearer ${googleToken}`;
+  }
 
-    if ((accessToken) !== null) {
-        console.log(accessToken)
-        config.headers.commen["Authorization"] = `Bearer ${accessToken}`;
-    }
-    return config;
-})
+  if (accessToken !== null) {
+    console.log(accessToken);
+    config.headers.common["Authorization"] = `Bearer ${accessToken}`;
+  }
+  return config;
+});
 
 export const apis = {
-    signup: (id, email, pw) => api.post('/api/users/signup', {
-        username: email,
-        nickname: id,
-        password: pw,
+  signup: (id, email, pw) =>
+    api.post("/api/users/signup", {
+      username: email,
+      nickname: id,
+      password: pw,
     }),
-    login: (email, pw) => api.post('/api/users/login', {
+  signIn: (email, password) =>
+    api.post(
+      "/api/users/login",
+      {
         username: email,
-        password: pw,
-    },
-        { withCredentials: true }),
-    GoogleLogin : (userInfo) => api.post('/api/users/login/google', userInfo)
-}
+        password: password,
+      }
+      //   { withCredentials: true }
+    ),
+  GoogleLogin: (userInfo) => api.post("/api/users/login/google", userInfo),
+};
