af39c0de8939ca60f6cef8b13c90c037f54ceb4b
JoyKim66 <masscom6051@naver.com>
2022-07-14 17:36:30 +0900

    difficulty 에 따라 코인, 경험치 변경되도록 수정

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {useDispatch,useSelector} from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { actionCreators as todoAction } from "../redux/modules/todo";
 import styled from "styled-components";
 import DateRange from "../components/Write/DateRange";
@@ -10,87 +10,90 @@ import WriteHeader from "../components/Write/WriteHeader";
 import CommonModal from "../element/CommonModal";
 
 const Write = () => {
-    const dispatch = useDispatch();
-    const todoId = useParams().id;
+  const dispatch = useDispatch();
+  const todoId = useParams().id;
 
-    //todo 수정 조회 데이터
-    const editTodoObj = useSelector(state => state.todo.EditTodo);
-    console.log("editTodoObj:::",editTodoObj);
+  //todo 수정 조회 데이터
+  const editTodoObj = useSelector((state) => state.todo.EditTodo);
+  console.log("editTodoObj:::", editTodoObj);
 
-    const [todoType,setTodoType] = useState(null);
-    const [todoText,setTodoText] = useState(null);
-    const [diff,setDiff] = useState(null);
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+  const [todoType, setTodoType] = useState(null);
+  const [todoText, setTodoText] = useState(null);
+  const [diff, setDiff] = useState(null);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-    const convertedStartDate =  startDate.toISOString().split('T')[0]
-    const convertedEndDate = endDate.toISOString().split('T')[0]
-    
+  const convertedStartDate = startDate.toISOString().split("T")[0];
+  const convertedEndDate = endDate.toISOString().split("T")[0];
 
-    const [modal,setModal] = useState(false);
-    const [modalText,setModalText] = useState(null);
-    const openErrModal = (modalText) => {
-        setModal(true);
-        setModalText(modalText)
+  const [modal, setModal] = useState(false);
+  const [modalText, setModalText] = useState(null);
+  const openErrModal = (modalText) => {
+    setModal(true);
+    setModalText(modalText);
+  };
+  const onConfirm = () => {
+    setModal(false);
+  };
+
+  useEffect(() => {
+    {
+      todoId && dispatch(todoAction.getEditTodolistDB(todoId));
     }
-    const onConfirm = () => {
-        setModal(false);
-    }
+  }, []);
+  useEffect(() => {
+    setTodoType(editTodoObj.todoType);
+    setTodoText(editTodoObj.content);
+    setDiff(editTodoObj.difficulty);
+  }, [editTodoObj]);
 
-    useEffect(()=>{
-        {todoId &&
-        dispatch(todoAction.getEditTodolistDB(todoId))
-        }
-    },[]) 
-    useEffect(()=>{
-        setTodoType(editTodoObj.todoType)
-        setTodoText(editTodoObj.content)
-        setDiff(editTodoObj.difficulty)
-    },[editTodoObj])
-
-    
-
-    if (Object.keys(editTodoObj).length === 0 && todoId) return null; 
-    return(
-        <>
-        <WriteContainer>
-            <CommonModal 
-                title={"notice"}
-                visible={modal}
-                modalText={modalText}
-                onConfirm={onConfirm}
-                isSingleBtn
-                />
-            <WriteHeader 
-            todoObj={{todoType,todoText,diff,convertedStartDate,convertedEndDate}} 
-            openErrModal = {openErrModal}
-            onConfirm = {onConfirm}
-            />
-            <TodoWrite 
-            setTodoType={setTodoType} 
-            setTodoText={setTodoText}
-            todoText={todoText}
-            todoType={todoType}
-            />
-            <Difficulty setDiff={setDiff} diff={diff}/>
-            {/* 수정페이지에서는 DATE수정불가 */}
-            {!todoId &&
-            <DateRange 
-            startDate={startDate} 
-            setStartDate={setStartDate} 
+  if (Object.keys(editTodoObj).length === 0 && todoId) return null;
+  return (
+    <>
+      <WriteContainer>
+        <CommonModal
+          title={"notice"}
+          visible={modal}
+          modalText={modalText}
+          onConfirm={onConfirm}
+          isSingleBtn
+        />
+        <WriteHeader
+          todoObj={{
+            todoType,
+            todoText,
+            diff,
+            convertedStartDate,
+            convertedEndDate,
+          }}
+          openErrModal={openErrModal}
+          onConfirm={onConfirm}
+        />
+        <TodoWrite
+          setTodoType={setTodoType}
+          setTodoText={setTodoText}
+          todoText={todoText}
+          todoType={todoType}
+        />
+        <Difficulty setDiff={setDiff} diff={diff} todoType={todoType} />
+        {/* 수정페이지에서는 DATE수정불가 */}
+        {!todoId && (
+          <DateRange
+            startDate={startDate}
+            setStartDate={setStartDate}
             endDate={endDate}
-            setEndDate={setEndDate} 
-            />
-            }
-        </WriteContainer>
-        </>
-    )
-}
+            setEndDate={setEndDate}
+          />
+        )}
+      </WriteContainer>
+    </>
+  );
+};
 
 export default Write;
 
 const WriteContainer = styled.div`
-    min-width: 360px;
-    height: 640px;
-    background: #FFFFFF;
+  min-width: 360px;
+  height: 640px;
+  background: #ffffff;
 `;
